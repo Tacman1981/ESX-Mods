dbafbcd983622cad3138f25c9885d579295331e8
Tacman1981 <101606905+Tacman1981@users.noreply.github.com>
2025-03-03 04:32:34 +0000

    Update config.lua

--- luckywheel/config.lua
+++ luckywheel/config.lua
@@ -4,17 +4,17 @@ Config.Cooldown = 1800000 -- Cooldown duration in milliseconds (30 minutes)
 
 Config.Prizes = {
     {name = "$50,000", money = 50000, weight = 1},
-    {name = "$20,000", money = 20000, weight = 2},
-    {name = "$15,000", money = 15000, weight = 3},
-    {name = "$10,000", money = 10000, weight = 4},
-    {name = "$7,500", money = 7500, weight = 5},
-    {name = "$5,000", money = 5000, weight = 6},
-    {name = "$3,000", money = 3000, weight = 7},
-    {name = "$2,000", money = 2000, weight = 8},
-    {name = "$1,000", money = 1000, weight = 9},
-    {name = "Bagged Cocaine", money = 0, item = "coke_pooch", minQuantity = 50, maxQuantity = 1000, weight = 10},
-    {name = "Amfetamine Pooch", money = 0, item = "amfe_pooch", minQuantity = 50, maxQuantity = 1000, weight = 10},
-    {name = "Marijuana", money = 0, item = "marijuana", minQuantity = 50, maxQuantity = 1000, weight = 10},
-    {name = "Mystery Cash Prize", money = 0, minMoney = 10000, maxMoney = 500000, weight = 20}
+    {name = "$20,000", money = 20000, weight = 3},
+    {name = "$15,000", money = 15000, weight = 6},
+    {name = "$10,000", money = 10000, weight = 10},
+    {name = "$7,500", money = 7500, weight = 15},
+    {name = "$5,000", money = 5000, weight = 21},
+    {name = "$3,000", money = 3000, weight = 28},
+    {name = "$2,000", money = 2000, weight = 36},
+    {name = "$1,000", money = 1000, weight = 45},
+    {name = "Bagged Cocaine", money = 0, item = "coke_pooch", minQuantity = 50, maxQuantity = 1000, weight = 55},
+    {name = "Amfetamine Pooch", money = 0, item = "amfe_pooch", minQuantity = 50, maxQuantity = 1000, weight = 65},
+    {name = "Marijuana", money = 0, item = "marijuana", minQuantity = 50, maxQuantity = 1000, weight = 75},
+    {name = "Mystery Cash Prize", money = 0, minMoney = 10000, maxMoney = 500000, weight = 85}
 
 }
